4dc275c84e7ae2cc9d2cc071272c3764ddab8010
Kruthartha S Gowda <krutharthasgowda@gmail.com>
2025-08-15 12:19:41 +0530

    Fixed path in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />          {/* Home page */}
-        <Route path="/about" element={<Home />} />    {/* About page */}
+        <Route path="/" element={<Home />} /> {/* Home page */}
+        <Route path="/about" element={<Home />} /> {/* About page */}
         <Route path="/dashboard" element={<Home />} /> {/* Dashboard */}
-        <Route path="*" element={<NotFound />} />     {/* 404 Page */}
+        <Route path="*" element={<NotFound />} /> {/* 404 Page */}
       </Routes>
     </Router>
   );
